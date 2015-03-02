mirrorscotty/fe-solver

commit ff68ae8fa896a7115097ea267db0ce28286afde8

Assorted fixes for solving multiple PDEs
* Currently, applying Neumann boundary conditions is broken and causes
    the nonlinear solver to fail to converge.
* Fixed matrix assembly so that two (or hopefully more) equations are
    put together correctly.
* Added an stability improvement for Dirichlet boundary conditions for
    transient problems.
* Made the output look a bit better.

problems/slab-drying/common.c
@@ -200,6 +200,9 @@ void ApplyAllBCs(struct fe1d *p)
     else
         ApplyEssentialBC1D(p, PVAR, &IsOnRightBoundary, &ExternalConc);
 #endif
+    //mtxprnt(p->F);
+    //mtxprnt(p->J);
+    //exit(0);
     return;
 }
 

solver/finite-element1d.c
@@ -125,25 +125,29 @@ matrix* AssembleJ1D(struct fe1d *problem, matrix *guess)
     b = problem->b;
 
     matrix *J, *j;
-    int i, x, y;
-    int c, d;
+    int i,
+        x, y,
+        c, d,
+        vx, vy;
 
-    double rows = problem->nrows*problem->nvars;
+    int rows = problem->nrows,
+        vars = problem->nvars;
 
     if(!guess)
-        guess = CreateMatrix(rows, 1);
+        guess = CreateMatrix(rows*vars, 1);
 
-    J = CreateMatrix(rows, rows);
+    J = CreateMatrix(rows*vars, rows*vars);
 
     for(i=0; i<mesh->nelem; i++) {
-        j = problem->makej(problem, mesh->elem[i], guess);
+        j = problem->makedj(problem, mesh->elem[i], guess);
 
         for(x=0; x<b->n; x++) {
             for(y=0; y<b->n; y++) {
                 c = valV(mesh->elem[i]->map, x);
                 d = valV(mesh->elem[i]->map, y);
-
-                addval(J, val(j, x, y), c, d);
+                for(vx=0; vx<vars; vx++)
+                    for(vy=0; vy<vars; vy++)
+                        addval(J, val(j, x*vars+vx, y*vars+vy), c*vars+vx, d*vars+vy);
             }
         }
         DestroyMatrix(j);
@@ -172,15 +176,18 @@ matrix* AssembledJ1D(struct fe1d *problem, matrix *guess)
     b = problem->b;
 
     matrix *dJ, *dj;
-    int i, x, y;
-    int c, d;
+    int i,
+        x, y,
+        c, d,
+        vx, vy;
 
-    double rows = problem->nrows*problem->nvars;
+    int rows = problem->nrows,
+        vars = problem->nvars;
 
     if(!guess)
-        guess = CreateMatrix(rows, 1);
+        guess = CreateMatrix(rows*vars, 1);
 
-    dJ = CreateMatrix(rows, rows);
+    dJ = CreateMatrix(rows*vars, rows*vars);
 
     for(i=0; i<mesh->nelem; i++) {
         dj = problem->makedj(problem, mesh->elem[i], guess);
@@ -189,8 +196,9 @@ matrix* AssembledJ1D(struct fe1d *problem, matrix *guess)
             for(y=0; y<b->n; y++) {
                 c = valV(mesh->elem[i]->map, x);
                 d = valV(mesh->elem[i]->map, y);
-
-                addval(dJ, val(dj, x, y), c, d);
+                for(vx=0; vx<vars; vx++)
+                    for(vy=0; vy<vars; vy++)
+                        addval(dJ, val(dj, x*vars+vx, y*vars+vy), c*vars+vx, d*vars+vy);
             }
         }
         DestroyMatrix(dj);
@@ -209,6 +217,7 @@ matrix* AssembledJ1D(struct fe1d *problem, matrix *guess)
  * @param problem Finite element problem structure
  * @param guess Estimated values of the dependent variables at the current
  *      time step.
+ * TODO: Broken with multiple variables. Fix it!
  */
 matrix *AssembleF1D(struct fe1d *problem, matrix *guess)
 {
@@ -216,23 +225,25 @@ matrix *AssembleF1D(struct fe1d *problem, matrix *guess)
     basis *b = problem->b;
 
     matrix *F, *f;// *lguess;
-    int i, j, rows;
-    int r = b->n - b->overlap;
+    int i, x, c, rows, vars, vx;
 
     rows = problem->nrows;
+    vars = problem->nvars;
 
-    F = CreateMatrix(rows, 1);
-
-    for(i=0; i<rows-r; i=i+r) {
-
+    F = CreateMatrix(rows*vars, 1);
+/*
+    for(i=0; i<mesh->nelem; i++) {
         f = problem->makef(problem, mesh->elem[i], guess);
 
-        for(j=0; j<b->n; j++) {
-            addval(F, val(f, j, 0), i+j, 0);
+        for(x=0; x<b->n*vars; x++) {
+            c = valV(mesh->elem[i]->map, x);
+            for(vx=0; vx<vars; vx++)
+                addval(F, val(f, x+vx, 0), c+vx, 0);
         }
-
         DestroyMatrix(f);
     }
+
+    */
     problem->F = F;
 
     return F;
@@ -281,6 +292,7 @@ void FE1DTransInit(struct fe1d *problem, matrix* InitSoln)
     /* Generate the appropriate load vector */
     AssembleF1D(problem, InitSoln);
 
+    puts("");
     /* Apply the boundary conditions again to make sure the load vector is
      * properly initialized */
     problem->applybcs(problem);
@@ -314,20 +326,34 @@ void ApplyEssentialBC1D(struct fe1d* p,
         int (*cond)(struct fe1d*, int),
         double (*BC)(struct fe1d*, int))
 {
-    int i, j;
+    int i, j, k;
+    double value;
+    solution *s;
     /* Loop through the rows */
-    for(i=var; i<p->nrows*p->nvars; i+=p->nvars) {
+    for(i=0; i<p->nrows; i++) {
         /* Check to see if the BC should be applied */
         if(cond(p, i)) {
             //printf("Applying Dirchlet boundary condition at node %d for variable %d\n", i/p->nvars, var);
+            k = i*p->nvars+var;// printf("k = %d\n", k);
             /* Zero out the row */
             for(j=0; j<p->nrows*p->nvars; j++) {
-                setval(p->J, (i==j)?1:0, i, j); /* Use the Chroniker delta */
-                setval(p->dJ, (i==j)?1:0, i, j); /* Use the Chroniker delta */
-                //setval(p->dJ, 0, i, j);
+                setval(p->J, (k==j)?1:0, k, j); /* Use the Chroniker delta */
+                setval(p->dJ, (k==j)?1:0, k, j); /* Use the Chroniker delta */
             }
             /* Set the appropriate value in the load vector */
-            setval(p->F, BC(p, i), i, 0);
+            if(p->t == 1) {
+                /* If this is the first time step, set the boundary value to
+                 * halfway between the initial value and what it should be. This
+                 * should increase numerical stability. */
+                s = FetchSolution(p, 0);
+                value = (BC(p, k) + val(s->val, k, 0))/2;
+            } else {
+                /* For the other time steps, just set it to the correct
+                 * value. */
+                value = BC(p, k);
+            }
+
+            setval(p->F, value, k, 0);
         }
     }
     return;
@@ -346,10 +372,10 @@ void ApplyNaturalBC1D(struct fe1d *p,
         double (*BC)(struct fe1d*, int))
 {
     int i;
-    for(i=var; i<p->nrows*p->nvars; i+=p->nvars) {
+    for(i=0; i<p->nrows; i++) {
         if(cond(p, i))
             //printf("Applying Neumann boundary condition at node %d for variable %d\n", i/p->nvars, var);
-            addval(p->F, BC(p, i), i, 0);
+            addval(p->F, BC(p, i), i*p->nvars+var, 0);
     }
 }
 
@@ -483,6 +509,11 @@ solution* FetchSolution(struct fe1d *p, int t)
         return NULL;
 }
 
+double FetchGuessValue(struct fe1d *p, int node, int var)
+{
+    return val(p->guess, node*p->nvars+var, 0);
+}
+
 /**
  * Evaluate a one-dimensional solution at a particular local coordinate within
  * a specific element. This interpolates the already calculated solution using
@@ -592,9 +623,21 @@ double EvalSoln1DG(struct fe1d *p, int var, solution *s, double x, int coord)
  */
 void PrintSolution(struct fe1d *p, int t)
 {
+    int i, j;
     solution *s;
+    matrix *output;
+
     s = FetchSolution(p, t);
-    mtxprnt(s->val);
+    output = CreateMatrix(nRows(s->val)/p->nvars, p->nvars);
+
+    for(i=0; i<nRows(output); i++)
+        for(j=0; j<nCols(output); j++)
+            setval(output, val(s->val, p->nvars*i+j, 0), i, j);
+
+    mtxprnt(output);
+    DestroyMatrix(output);
+
+    return;
 }
 
 /**

Makefile
@@ -4,7 +4,7 @@ VPATH=problems problems/slab-drying gui solver/mesh solver/ode solver/integratio
 
 OBJECTFILES=integrate.o basis.o mesh2d.o finite-element.o isoparam.o finite-element1d.o mesh1d.o solution.o auxsoln.o scaling_ht.o linsolve1d.o nlinsolve1d.o predict1d.o linsolve2d.o nlinsolve2d.o output.o material-data.a matrix.a
 
-all: heat-transfer
+all: diffusion
 
 doc:
 	doxygen DoxyFile
@@ -12,28 +12,28 @@ doc:
 	cp doc/latex/refman.pdf doc/Reference.pdf
 
 2dlaplace: 2dlaplace.o $(OBJECTFILES)
-	gcc -o 2dlaplace 2dlaplace.o $(OBJECTFILES) $(CFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 ce675p1: ce675p1.o $(OBJECTFILES)
-	$(CC) -o ce675p1 ce675p1.o $(OBJECTFILES) $(CFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 spheroid: spheroid.o $(OBJECTFILES)
-	$(CC) -o spheroid spheroid.o $(OBJECTFILES) $(CFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 ce675p2: ce675p2.o $(OBJECTFILES)
-	$(CC) -o ce675p2 ce675p2.o $(OBJECTFILES) $(CFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 heat-explicit: heat-explicit.o $(OBJECTFILES)
-	$(CC) -o heat-explicit heat-explicit.o $(OBJECTFILES) $(CFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 heat-cyl: heat-cyl.o heat-gui.o $(OBJECTFILES)
-	$(CC) -o heat-cyl heat-cyl.o heat-gui.o $(OBJECTFILES) $(CFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 heat-transfer: heat-transfer.o main.o common.o $(OBJECTFILES)
-	$(CC) -o heat-transfer heat-transfer.o main.o common.o $(OBJECTFILES) $(CFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 diffusion: diffusion.o heat-transfer.o main.o common.o $(OBJECTFILES)
-	$(CC) -o $@ $? $(CFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS)
 
 clean:
 #rm -rf spheroid 2dlaplace ce675p1 ce675p2 heat-explicit heat-cyl meshtest

problems/slab-drying/diffusion.c
@@ -86,7 +86,7 @@ double ResMass(struct fe1d *p, matrix *guess, Elem1D *elem,
         Ci = EvalSoln1D(p, CVAR, elem, s, valV(elem->points, i));
         //Di = DIFF(uscaleTemp(p->chardiff, Ci), uscaleTemp(p->charvals, Ti));
         Di = DIFF(uscaleTemp(p->chardiff, Ci), TINIT);
-        printf("Di = %g\n", Di);
+        //printf("Di = %g\n", Di);
         D += Di * b->phi[i](x);
         DDDx += Di * b->dphi[i](x);
     }
@@ -170,12 +170,14 @@ double ConvBCMass(struct fe1d *p, int row)
     double Cinf = ExternalConc(p, row);
     double C;
     double Bi = BiotNumber(p->chardiff);
+    //Bi = 10;
 
     /* Calculate the convective boundary condition based on the current estimate
      * for the temperature at the boundary. */
     if(p->guess) {
-        C = val(p->guess, row, 0);
-        return Bi*(C-Cinf);
+        C = FetchGuessValue(p, row, CVAR);
+        printf("C = %g; Cinf = %g\n", C, Cinf);
+        return -1*Bi*(C-Cinf);
     } else {
         return 0;
     }

problems/slab-drying/heat-transfer.c
@@ -193,7 +193,7 @@ double ConvBCHeat(struct fe1d *p, int row)
     /* Calculate the convective boundary condition based on the current estimate
      * for the temperature at the boundary. */
     if(p->guess) {
-        T = val(p->guess, row, 0);
+        T = FetchGuessValue(p, row, TVAL);
         return -Bi*(T-Tinf);
     } else {
         return 0;

problems/slab-drying/main.c
@@ -34,14 +34,16 @@ int main(int argc, char *argv[])
 
     comp_global = CreateChoiOkos(0, 0, 0, 1, 0, 0, 0);
     scale_heat = SetupScaling(alpha(comp_global, TINIT), TINIT, TAMB, THICKNESS, k(comp_global, TINIT), HCONV);
-    scale_mass = SetupScaling(DIFF(CINIT, TINIT), CINIT, CAMB, THICKNESS, DIFF(CINIT, TINIT), KC_CONV);
+    //scale_heat = SetupScaling(1, TINIT, TAMB, 1, 1, HCONV);
+    scale_mass = SetupScaling(DIFF(CINIT, TINIT), CINIT, CAMB, THICKNESS, DIFF(CINIT, TINIT), KC_CONV/10);
+    //scale_mass = SetupScaling(1, CINIT, CAMB, 1, 1, HCONV);
 
     /* Make a linear 1D basis */
     b = MakeLinBasis(1);
 
     /* Create a uniform mesh */
-    mesh = GenerateUniformMesh1D(b, 0.0, scaleLength(scale_heat, THICKNESS), 10);
-    //mesh = GenerateUniformMesh1D(b, 0.0, scaleLength(scale_mass, THICKNESS), 10);
+    //mesh = GenerateUniformMesh1D(b, 0.0, scaleLength(scale_heat, THICKNESS), 2);
+    mesh = GenerateUniformMesh1D(b, 0.0, scaleLength(scale_mass, THICKNESS), 10);
     
     problem = CreateFE1D(b, mesh,
                          &CreateDTimeMatrix,
@@ -50,7 +52,7 @@ int main(int argc, char *argv[])
                          &ApplyAllBCs,
                          1000);
     problem->nvars = 2; /* Number of simultaneous PDEs to solve */
-    problem->dt = 0.001; /* Dimensionless time step size */
+    problem->dt = .001; /* Dimensionless time step size */
     problem->charvals = scale_heat;
     problem->chardiff = scale_mass;
 
@@ -69,6 +71,10 @@ int main(int argc, char *argv[])
 
     while(problem->t<problem->maxsteps) {
         NLinSolve1DTransImp(problem, NULL);
+        //mtxprnt(problem->J);
+        //mtxprnt(problem->F);
+        //mtxprnt(problem->dJ);
+        //exit(0);
         if(problem->t-1 > 0)
             problem->mesh = 
                 MoveMeshF(problem, problem->mesh->orig,
@@ -79,8 +85,8 @@ int main(int argc, char *argv[])
     PrintScalingValues(problem->chardiff);
 
     printf("Solution at t = %g:\n", uscaleTime(problem->charvals, problem->t*problem->dt));
-    printf("Solution at t = %g:\n", uscaleTime(problem->chardiff, problem->t*problem->dt));
     PrintSolution(problem, 1);
+    printf("Solution at t = %g:\n", uscaleTime(problem->chardiff, problem->t*problem->dt));
     PrintSolution(problem, problem->t-1);
 
     CSVOutFixedNode2(problem, 0, "output00.csv");

solver/nlinsolve1d.c
@@ -27,6 +27,11 @@ int CheckConverg1D(struct fe1d *problem, matrix *dx)
     for(i=0; i<rows; i++) {
         if(fabs(val(dx, i, 0)) > problem->tol)
             return 0;
+        if(isnan(val(dx, i, 0))) {
+            printf("Nonlinear solver failed to converge. Solver returned value of \"NaN\".\nFailed to calculate solution at time step %d of %d\nExiting.\n",
+                problem->t, problem->maxsteps);
+            exit(0);
+        }
     }
     return 1;
 }
@@ -159,9 +164,10 @@ matrix* NLinSolve1DTransImp(struct fe1d *problem, matrix *guess)
 
     /* Predict the next solution if an initial guess isn't supplied. */
     if(!guess)
-        guess = PredictSolnO2(problem);
+        guess = PredictSolnO1(problem);
 
     dx = NULL;
+    //exit(0);
 
     do {
         iter++;

commit b9bb7bf783cf146721444290304ef00b2dfb81d5

Fixed bug in matrix assembly
* A regression introduced in commit ff68ae8 was fixed. Previously, both
    the J and dJ matricies were using the same function to assemble
    themselves. This has been corrected.
* Made separate main files for heat and mass transfer models.

problems/slab-drying/common.c
@@ -220,7 +220,6 @@ void ApplyAllBCs(struct fe1d *p)
  */
 double DeformationGrad(struct fe1d *p, double X, double t)
 {
-    return 1;
     solution *s0, *sn;
     double rho0, rhon;
     double T0 = TINIT, Tn = TINIT;
@@ -254,7 +253,6 @@ double DeformationGrad(struct fe1d *p, double X, double t)
     rho0 = rho(comp_global, T0);
 #endif
 
-    //return 1;
     return rho0/rhon;
 }
 

problems/slab-drying/ht-main.c
@@ -1,0 +1,96 @@
+/**
+ * @file heat-cyl.c
+ * Console-only front end to the can sterilization model. The rest of the code
+ * is in gui/heating
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "matrix.h"
+#include "basis.h"
+#include "integrate.h"
+#include "mesh1d.h"
+#include "isoparam.h"
+#include "finite-element1d.h"
+#include "auxsoln.h"
+#include "solve.h"
+
+#include "output.h"
+#include "material-data.h"
+
+#include "common.h"
+
+choi_okos *comp_global;
+
+int main(int argc, char *argv[])
+{
+    Mesh1D *mesh;
+    basis *b;
+    matrix *IC_heat;
+    struct fe1d* problem;
+    scaling_ht scale_heat;
+
+    comp_global = CreateChoiOkos(0, 0, 0, 1, 0, 0, 0);
+    scale_heat = SetupScaling(alpha(comp_global, TINIT), TINIT, TAMB, THICKNESS, k(comp_global, TINIT), HCONV);
+    //scale_heat = SetupScaling(1, TINIT, TAMB, 1, 1, HCONV);
+
+    /* Make a linear 1D basis */
+    b = MakeLinBasis(1);
+
+    /* Create a uniform mesh */
+    mesh = GenerateUniformMesh1D(b, 0.0, scaleLength(scale_heat, THICKNESS), 10);
+    
+    problem = CreateFE1D(b, mesh,
+                         &CreateDTimeMatrix,
+                         &CreateElementMatrix,
+                         &CreateElementLoad,
+                         &ApplyAllBCs,
+                         100);
+    problem->nvars = 1; /* Number of simultaneous PDEs to solve */
+    problem->dt = 0.01; /* Dimensionless time step size */
+    problem->charvals = scale_heat;
+
+    /* Set the initial temperature */
+    IC_heat = GenerateInitCondConst(problem, TVAR, scaleTemp(problem->charvals, TINIT));
+
+    /* Apply the initial condition to the problem and set up the transient
+     * solver. */
+    FE1DTransInit(problem, IC_heat);
+
+    while(problem->t<problem->maxsteps) {
+        NLinSolve1DTransImp(problem, NULL);
+        if(problem->t-1 > 0)
+            problem->mesh = 
+                MoveMeshF(problem, problem->mesh->orig,
+                          problem->t-1, &DeformationGrad);
+    }
+
+    PrintScalingValues(problem->charvals);
+
+    printf("Solution at t = %g:\n", uscaleTime(problem->charvals, problem->t*problem->dt));
+    PrintSolution(problem, 1);
+    puts("");
+    PrintSolution(problem, problem->t-1);
+
+    CSVOutFixedNode2(problem, 0, "output00.csv");
+    CSVOutFixedNode2(problem, 1, "output01.csv");
+    CSVOutFixedNode2(problem, 2, "output02.csv");
+    CSVOutFixedNode2(problem, 3, "output03.csv");
+    CSVOutFixedNode2(problem, 4, "output04.csv");
+    CSVOutFixedNode2(problem, 5, "output05.csv");
+    CSVOutFixedNode2(problem, 6, "output06.csv");
+    CSVOutFixedNode2(problem, 7, "output07.csv");
+    CSVOutFixedNode2(problem, 8, "output08.csv");
+    CSVOutFixedNode2(problem, 9, "output09.csv");
+    CSVOutFixedNode2(problem, 10, "output10.csv");
+
+    PrintVector(problem->mesh->orig->nodes);
+    PrintVector(problem->mesh->nodes);
+
+    /* Clean up */
+    DestroyFE1D(problem);
+
+    return 0;
+}
+

problems/slab-drying/mt-main.c
@@ -1,0 +1,93 @@
+/**
+ * @file mt-main.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "matrix.h"
+#include "basis.h"
+#include "integrate.h"
+#include "mesh1d.h"
+#include "isoparam.h"
+#include "finite-element1d.h"
+#include "auxsoln.h"
+#include "solve.h"
+
+#include "output.h"
+#include "material-data.h"
+
+#include "common.h"
+
+choi_okos *comp_global;
+
+int main(int argc, char *argv[])
+{
+    Mesh1D *mesh;
+    basis *b;
+    matrix *IC_mass;
+    struct fe1d* problem;
+    scaling_ht scale_mass;
+
+    comp_global = CreateChoiOkos(0, 0, 0, 1, 0, 0, 0);
+    scale_mass = SetupScaling(DIFF(CINIT, TINIT), CINIT, CAMB, THICKNESS, DIFF(CINIT, TINIT), KC_CONV);
+    //scale_mass = SetupScaling(1, CINIT, CAMB, 1, 1, HCONV);
+
+    /* Make a linear 1D basis */
+    b = MakeLinBasis(1);
+
+    /* Create a uniform mesh */
+    mesh = GenerateUniformMesh1D(b, 0.0, scaleLength(scale_mass, THICKNESS), 10);
+    
+    problem = CreateFE1D(b, mesh,
+                         &CreateDTimeMatrix,
+                         &CreateElementMatrix,
+                         &CreateElementLoad,
+                         &ApplyAllBCs,
+                         2);
+    problem->nvars = 1; /* Number of simultaneous PDEs to solve */
+    problem->dt = .001; /* Dimensionless time step size */
+    problem->chardiff = scale_mass;
+
+    /* Set the initial temperature */
+    IC_mass = GenerateInitCondConst(problem, CVAR, scaleTemp(problem->chardiff, CINIT));
+
+    /* Apply the initial condition to the problem and set up the transient
+     * solver. */
+    FE1DTransInit(problem, IC_mass);
+
+    while(problem->t<problem->maxsteps) {
+        NLinSolve1DTransImp(problem, NULL);
+        if(problem->t-1 > 0)
+            problem->mesh = 
+                MoveMeshF(problem, problem->mesh->orig,
+                          problem->t-1, &DeformationGrad);
+    }
+
+    PrintScalingValues(problem->chardiff);
+
+    PrintSolution(problem, 1);
+    printf("Solution at t = %g:\n", uscaleTime(problem->chardiff, problem->t*problem->dt));
+    PrintSolution(problem, problem->t-1);
+
+    CSVOutFixedNode2(problem, 0, "output00.csv");
+    CSVOutFixedNode2(problem, 1, "output01.csv");
+    CSVOutFixedNode2(problem, 2, "output02.csv");
+    CSVOutFixedNode2(problem, 3, "output03.csv");
+    CSVOutFixedNode2(problem, 4, "output04.csv");
+    CSVOutFixedNode2(problem, 5, "output05.csv");
+    CSVOutFixedNode2(problem, 6, "output06.csv");
+    CSVOutFixedNode2(problem, 7, "output07.csv");
+    CSVOutFixedNode2(problem, 8, "output08.csv");
+    CSVOutFixedNode2(problem, 9, "output09.csv");
+    CSVOutFixedNode2(problem, 10, "output10.csv");
+
+    PrintVector(problem->mesh->orig->nodes);
+    PrintVector(problem->mesh->nodes);
+
+    /* Clean up */
+    DestroyFE1D(problem);
+
+    return 0;
+}
+

solver/finite-element1d.c
@@ -139,7 +139,7 @@ matrix* AssembleJ1D(struct fe1d *problem, matrix *guess)
     J = CreateMatrix(rows*vars, rows*vars);
 
     for(i=0; i<mesh->nelem; i++) {
-        j = problem->makedj(problem, mesh->elem[i], guess);
+        j = problem->makej(problem, mesh->elem[i], guess);
 
         for(x=0; x<b->n; x++) {
             for(y=0; y<b->n; y++) {
@@ -353,6 +353,7 @@ void ApplyEssentialBC1D(struct fe1d* p,
             }
 
             setval(p->F, value, k, 0);
+            //setval(p->F, BC(p, k), k, 0);
         }
     }
     return;