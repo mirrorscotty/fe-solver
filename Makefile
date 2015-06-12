ef4c01c14545a3489d60e4b98063572e1a47b53a
Alex Griessman <alex.griessman@gmail.com>
2015-06-12 11:50:16 -0400

    Updated the matrix module and deleted the one for material-data.
    A couple of output functions had to be commented out, but they really shouldn't
    be here anyway. They should go with the problem definition in that repository.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-CFLAGS=-lm -I. -Imatrix -Imaterial-data -Isolver -Ioutput -Isolver/mesh -Isolver/integration -Isolver/ode -Iscaling -Igui/heating -Wall -g3 -O0 
+CFLAGS=-lm -I. -Imatrix -Isolver -Ioutput -Isolver/mesh -Isolver/integration -Isolver/ode -Iscaling -Igui/heating -Wall -g3 -O0 
 VPATH=solver/mesh solver/ode solver/integration matrix scaling solver output
 
 OBJECTFILES=integrate.o basis.o mesh2d.o finite-element.o isoparam.o finite-element1d.o mesh1d.o solution.o auxsoln.o scaling_ht.o linsolve1d.o nlinsolve1d.o predict1d.o linsolve2d.o nlinsolve2d.o output.o stepsize.o
@@ -14,7 +14,6 @@ doc:
 clean:
 	rm -rf *.o *.a
 	$(MAKE) -C matrix clean
-	$(MAKE) -C material-data clean
 	rm -rf doc
 
 # Mesh-related files
--- output/output.c
+++ output/output.c
@@ -19,9 +19,9 @@
 #include "solver/ode/auxsoln.h"
 #include "scaling/scaling_ht.h"
 
-#include "material-data.h"
+//#include "material-data.h"
 
-extern choi_okos *comp_global;
+//extern choi_okos *comp_global;
 
 /**
  * @brief Output bunches of data for a single node
@@ -33,39 +33,39 @@ extern choi_okos *comp_global;
  * @param row The number of the row (node) to output data for
  * @param filename The name of the file to spit stuff out into
  */
-void CSVOutFixedNode(struct fe1d *p, int row, char *filename)
-{
-    int i;
-    FILE *fp;
-    solution *s;
-    double T;
-
-    fp = fopen(filename, "w+");
-
-    if(!fp) {
-        fprintf(stderr, "Unable to open file for writing.");
-        return;
-    }
-
-    /* Print out the column headers */
-    fprintf(fp, "Time,Temperature,ProdConc,IceMassFrac,Density,HeatCapacity,ThermalConductivity\n");
-
-    /* Print out the values */
-    for(i=0; i<p->maxsteps; i++) {
-        s = FetchSolution(p, i);
-        T = uscaleTemp(p->charvals, val(s->val, row, 0));
-        fprintf(fp, "%g,%g,", uscaleTime(p->charvals, i*p->dt), T);
-        //s = FetchAuxSoln(p, 0, i);
-        //fprintf(fp, "%g,", val(s->val, row, 0));
-       // fprintf(fp, "%g,",  IceMassFrac(T));
-        fprintf(fp, "%g,%g,%g\n", rho(comp_global, T), Cp(comp_global, T), k(comp_global, T));
-    }
-    fprintf(fp, "\n");
-
-    fclose(fp);
-
-    return;
-}
+//void CSVOutFixedNode(struct fe1d *p, int row, char *filename)
+//{
+//    int i;
+//    FILE *fp;
+//    solution *s;
+//    double T;
+//
+//    fp = fopen(filename, "w+");
+//
+//    if(!fp) {
+//        fprintf(stderr, "Unable to open file for writing.");
+//        return;
+//    }
+//
+//    /* Print out the column headers */
+//    fprintf(fp, "Time,Temperature,ProdConc,IceMassFrac,Density,HeatCapacity,ThermalConductivity\n");
+//
+//    /* Print out the values */
+//    for(i=0; i<p->maxsteps; i++) {
+//        s = FetchSolution(p, i);
+//        T = uscaleTemp(p->charvals, val(s->val, row, 0));
+//        fprintf(fp, "%g,%g,", uscaleTime(p->charvals, i*p->dt), T);
+//        //s = FetchAuxSoln(p, 0, i);
+//        //fprintf(fp, "%g,", val(s->val, row, 0));
+//       // fprintf(fp, "%g,",  IceMassFrac(T));
+//        fprintf(fp, "%g,%g,%g\n", rho(comp_global, T), Cp(comp_global, T), k(comp_global, T));
+//    }
+//    fprintf(fp, "\n");
+//
+//    fclose(fp);
+//
+//    return;
+//}
 
 /**
  * @brief Output bunches of data for a single node
@@ -174,43 +174,43 @@ void CSVOutFixedNodeDx(struct fe1d *p, int row, char *filename)
  * @param tstep The time step to output data for
  * @param filename Where to save all the data.
  */
-void CSVOutFixedTime(struct fe1d *p, int tstep, char *filename)
-{
-    int i;
-    FILE *fp;
-    solution *T, *c1;
-
-    vector *defmesh;
-#ifdef CALC_ICE_FORMATION
-    defmesh = deformMesh(p, tstep);
-#else
-    defmesh = p->mesh->nodes;
-#endif
-
-    fp = fopen(filename, "w+");
-
-    if(!fp) {
-        fprintf(stderr, "Unable to open file for writing.");
-        return;
-    }
-
-    T = FetchSolution(p, tstep);
-    c1 = FetchAuxSoln(p, 0, tstep);
-
-    fprintf(fp, "Radius,Temperature,ProdConc,IceMassFrac,Density,HeatCapacity,ThermalConductivity\n");
-    for(i=0; i<p->nrows; i++)
-        fprintf(fp, "%g,%g,%g,%g,%g,%g\n",
-                uscaleLength(p->charvals, valV(defmesh, i)),
-                uscaleTemp(p->charvals, val(T->val, i, 0)),
-                val(c1->val, i, 0),
-               // IceMassFrac(uscaleTemp(p->charvals, val(T->val, i, 0))),
-                rho(comp_global, uscaleTemp(p->charvals, val(T->val, i, 0))),
-                Cp(comp_global, uscaleTemp(p->charvals, val(T->val, i, 0))),
-                k(comp_global, uscaleTemp(p->charvals, val(T->val, i, 0))));
-    fprintf(fp, "\n");
-
-    fclose(fp);
-
-    return;
-}
-
+//void CSVOutFixedTime(struct fe1d *p, int tstep, char *filename)
+//{
+//    int i;
+//    FILE *fp;
+//    solution *T, *c1;
+//
+//    vector *defmesh;
+//#ifdef CALC_ICE_FORMATION
+//    defmesh = deformMesh(p, tstep);
+//#else
+//    defmesh = p->mesh->nodes;
+//#endif
+//
+//    fp = fopen(filename, "w+");
+//
+//    if(!fp) {
+//        fprintf(stderr, "Unable to open file for writing.");
+//        return;
+//    }
+//
+//    T = FetchSolution(p, tstep);
+//    c1 = FetchAuxSoln(p, 0, tstep);
+//
+//    fprintf(fp, "Radius,Temperature,ProdConc,IceMassFrac,Density,HeatCapacity,ThermalConductivity\n");
+//    for(i=0; i<p->nrows; i++)
+//        fprintf(fp, "%g,%g,%g,%g,%g,%g\n",
+//                uscaleLength(p->charvals, valV(defmesh, i)),
+//                uscaleTemp(p->charvals, val(T->val, i, 0)),
+//                val(c1->val, i, 0),
+//               // IceMassFrac(uscaleTemp(p->charvals, val(T->val, i, 0))),
+//                rho(comp_global, uscaleTemp(p->charvals, val(T->val, i, 0))),
+//                Cp(comp_global, uscaleTemp(p->charvals, val(T->val, i, 0))),
+//                k(comp_global, uscaleTemp(p->charvals, val(T->val, i, 0))));
+//    fprintf(fp, "\n");
+//
+//    fclose(fp);
+//
+//    return;
+//}
+//
